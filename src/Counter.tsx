1498a6c41c1e20d58a8f51484bd89af91ce0c7c7
jay75chauhan <chauhan75jayk@gmail.com>
2022-01-07 15:50:33 +0530

    adding reducer

---
+++ src/Counter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface CounterProps {}
+
+export const Counter: React.FC<CounterProps> = ({}) => {
+  return (
+    <div>
+      <h1>Counter</h1>
+    </div>
+  );
+};
